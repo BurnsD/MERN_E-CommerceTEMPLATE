fa5c8c46773bf8b9547499c5891444c00dfc6395
BurnsD <Dustinburnsactech@gmail.com>
2022-12-02 22:07:55 -0600

    adding product list and photos for template

---
+++ frontend/src/data.js
@@ -0,0 +1,56 @@
+const data = {
+    products: [
+        {
+            name: 'Product 1',
+            slug: 'product-1',
+            category: 'category1',
+            image:'/images/p-acro-1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Fake',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'lorem ispum filler text do da do da',
+
+        },
+        {
+            name: 'Product 2',
+            slug: 'product-2',
+            category: 'category1',
+            image:'/images/p-acro-2.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Fake',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'lorem ispum filler text do da do da',
+
+        },
+        {
+            name: 'Product 3',
+            slug: 'product-3',
+            category: 'category1',
+            image:'/images/p-scoly-1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Fake',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'lorem ispum filler text do da do da',
+
+        },
+        {
+            name: 'Product 4',
+            slug: 'product-4',
+            category: 'category1',
+            image:'/images/p-scoly-2.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Fake',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'lorem ispum filler text do da do da',
+
+        },
+    ],
+};
