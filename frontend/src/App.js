ff85fca258f3ed48c86340211d83c87b45299f79
BurnsD <Dustinburnsactech@gmail.com>
2022-12-02 22:33:46 -0600

    added product anchors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,13 +11,21 @@ function App() {
       <main>
         <h1>Featured Products</h1>
         <div className="products">
-        {data.products.map((product) => (
-          <div className="product" key={product.slug}>
-            <img src={product.image} alt={product.name} />
-            <p>{product.name}</p>
-            <p>{product.price}</p>
-          </div>
-        ))}
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p><strong>${product.price}</strong></p>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
         </div>
       </main>
     </div>
