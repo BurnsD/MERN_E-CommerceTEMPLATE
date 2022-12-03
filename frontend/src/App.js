03d34271832ebbc3d242599b7dc969039fc89002
BurnsD <Dustinburnsactech@gmail.com>
2022-12-02 22:52:16 -0600

    added react router dom and moved featured products to homescreen route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import data from './data';
+
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+
 
 function App() {
   return (
+    <BrowserRouter>
     <div>
       <header>
         <a href="/">Website Name:</a>
       </header>
       <main>
-        <h1>Featured Products</h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-
-              <div className="product-info">
-                <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p><strong>${product.price}</strong></p>
-                <button>Add to Cart</button>
-              </div>
-            </div>
-          ))}
-        </div>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+        </Routes>
+       
       </main>
     </div>
+    </BrowserRouter>
   );
 }
 
